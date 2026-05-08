906c0d00b182c4ac05cc1e6d953156df42758c05
nnakirin <irashkikna@gmail.com>
2026-05-08 11:42:53 -0400

    Added login, logout, and registration features for React

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,13 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // 1. Додаємо імпорт компонента реєстрації
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      {/* 2. Додаємо маршрут для реєстрації */}
+      <Route path="/register" element={<Register />} /> 
     </Routes>
   );
 }
